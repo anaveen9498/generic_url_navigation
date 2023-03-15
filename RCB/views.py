f027bd5342e58e8ba0543c87c980dae25ef78d2a
anaveen9498 <anaveen9498@gmail.com>
2023-03-15 14:06:33 +0530

    first commit

---
+++ RCB/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def virat(request):
+    return render(request,'virat.html')
+
+
+def Naveen(request):
+    return render(request,'Naveen.html')
